3fa52cf23342ad98e79230b0f6d9ec34bf61e0f6
Khadidja <khadidjaarezki999@gmail.com>
2025-03-15 22:17:56 +0100

    Add auth and api slice reducers to RTK store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../reducers/authReducer'
 import usersReducer from './features/users/usersSlice'
+import { apiSlice } from '../reducers/apiSlice'
 import messagesReducer from './features/messages/messagesSlice'
 
 // eslint-disable-next-line import/prefer-default-export
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     user: usersReducer,
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
     messages: messagesReducer,
   },
+  // This required for RTK Query to cache results
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 })
+
+export default store
