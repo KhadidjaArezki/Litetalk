5120a8dbd08b79d6245f16d13923f529576f3de1
Khadidja <khadidjaarezki999@gmail.com>
2025-03-25 16:58:18 +0100

    multer expects a picture field in userRouter PUT request body instead of image

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -15,7 +15,7 @@ const router = Router()
 
 router.get('/', getUsersController)
 router.post('/', postUserController)
-router.put('/:id', upload.single('image'), putUserController)
+router.put('/:id', upload.single('picture'), putUserController)
 router.delete('/:id', deleteUserController)
 
 module.exports = router
