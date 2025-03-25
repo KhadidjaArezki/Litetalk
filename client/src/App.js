e57e3ad824ee23f3beca471b4d796e83f302c4ae
Khadidja <khadidjaarezki999@gmail.com>
2025-03-25 14:50:31 +0100

    Replace conditional rendering on auth by protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import Signup from './components/signup/Signup'
 import Chats from './components/chats/Chats'
 import ChatRoom from './components/chatroom/ChatRoom'
+import Signup from './components/signup/Signup'
 import Search from './components/search/Search'
 import Profile from './components/profile/Profile'
 import Friends from './components/friends/Friends'
-import MainPage from './components/main_page/MainPage'
 import EditProfilePage from './components/profile/EditProfilePage'
+import RequireAuth from './components/require_auth/RequireAuth'
 
 function App() {
-  const currentUser = useSelector(({ user }) => user)
-  console.log('Current User: ', currentUser)
-  const isLoggedIn = () => currentUser.username && currentUser.token
-
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route element={isLoggedIn() ? <MainPage /> : <Signup />}>
-            <Route path="/" element={<Chats />} />
-            <Route path="chats" element={<Chats />} />
-            <Route path="chatroom" element={<ChatRoom />} />
-            <Route path="search" element={<Search />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="edit" element={<EditProfilePage />} />
-            <Route path="friends" element={<Friends />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="search" element={<Search />} />
+          {/* Protected Routes */}
+          <Route element={<RequireAuth />}>
+            <Route exact path="/" element={<Chats />} />
+            <Route exact path="chats" element={<Chats />} />
+            <Route exact path="chatroom" element={<ChatRoom />} />
+            <Route exact path="profile" element={<Profile />} />
+            <Route exact path="edit" element={<EditProfilePage />} />
+            <Route exact path="friends" element={<Friends />} />
           </Route>
         </Routes>
       </Router>
