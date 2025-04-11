a2c8b368efc7864d7f90a005c1c21cc4fbe7baab
Khadidja <khadidjaarezki999@gmail.com>
2025-04-11 12:28:42 +0100

    Add ref to profile form to access form fields form parent

--- client/src/components/profile/ProfileForm.js
+++ client/src/components/profile/ProfileForm.js
@@ -1,33 +1,46 @@
+import { useState, forwardRef, useImperativeHandle } from 'react'
 import styles from '../../styles/Profile-styles/Profileform.module.css'
 
-function ProfileForm({ username, email, disable }) {
+const ProfileForm = forwardRef(({
+  username, email, disable,
+}, ref) => {
+  const [newUsername, setUsername] = useState(username)
+  const [newEmail, setEmail] = useState(email)
+
+  useImperativeHandle(ref, () => ({
+    newUsername,
+    newEmail,
+  }), [newUsername, newEmail])
+
   return (
     <form className={styles.Profile__form}>
-      <label htmlFor="user-name" className={styles['Profile__form-label']}>
+      <label htmlFor="username" className={styles['Profile__form-label']}>
         Name
         <input
           type="text"
-          name="name"
+          name="username"
           id="user-name"
-          defaultValue={username}
+          defaultValue={newUsername}
           className={styles['Profile__form-input']}
           readOnly={disable || false}
+          onChange={(event) => setUsername(event.target.value)}
         />
       </label>
 
-      <label htmlFor="user-email" className={styles['Profile__form-label']}>
+      <label htmlFor="email" className={styles['Profile__form-label']}>
         Email
         <input
           type="email"
           name="email"
           id="user-email"
-          defaultValue={email}
+          defaultValue={newEmail}
           className={styles['Profile__form-input']}
           readOnly={disable || false}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </label>
     </form>
   )
-}
+})
 
 export default ProfileForm
