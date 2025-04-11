810259e488bed986e168d4d246fff7d5c3b8e401
Khadidja <khadidjaarezki999@gmail.com>
2025-04-11 12:16:10 +0100

    Implement handleSave in edit profile page

--- client/src/components/profile/EditProfilePage.js
+++ client/src/components/profile/EditProfilePage.js
@@ -1,43 +1,65 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/self-closing-comp */
-import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import ProfileForm from './ProfileForm'
-import styles from '../../styles/Profile-styles/EditProfile.module.css'
-import Button from '../button/Button'
+import { useUpdateUserMutation } from '../../reducers/api/userApiSlice'
 import {
   selectCurrentId,
   selectCurrentUsername,
   selectCurrentEmail,
   selectCurrentPicture,
   selectCurrentFriends,
+  updateProfile,
 } from '../../reducers/authReducer'
+import ProfileForm from './ProfileForm'
+import styles from '../../styles/Profile-styles/EditProfile.module.css'
+import Button from '../button/Button'
+import { saveProfilePictureToDB } from '../../reducers/thunk/authThunks'
 
 function EditProfilePage() {
   const id = useSelector(selectCurrentId)
   const username = useSelector(selectCurrentUsername)
   const email = useSelector(selectCurrentEmail)
-  const { picture } = useSelector(selectCurrentPicture)
+  const { file } = useSelector(selectCurrentPicture)
   const friends = useSelector(selectCurrentFriends).map((f) => f.id)
 
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [updateUser] = useUpdateUserMutation()
+  const profileFormRef = useRef({})
 
-  const handleSave = (event) => {
-    // TODO: save the edit
+  const handleSave = async (event) => {
     event.preventDefault()
-    const newUsername = event.target.username.value
-    const newEmail = event.target.email.value
+    const { newUsername, newEmail } = profileFormRef.current
+
+    const pictureFileToSend = file?.data
+      ? new File(
+        [new Blob([new Uint8Array(file.data.data)], { type: file.contentType })],
+        `${username}.${file.contentType.substring(file.contentType.indexOf('/') + 1)}`,
+        { type: file.contentType },
+      )
+      : null
+    const updatedUser = await updateUser({
+      id,
+      username: newUsername,
+      email: newEmail,
+      friends,
+      picture: pictureFileToSend,
+    }).unwrap()
+    dispatch(saveProfilePictureToDB(updatedUser.picture))
+    dispatch(updateProfile(updatedUser))
+    navigate('/profile')
   }
 
   const handleCancel = () => {
-    // cancel the edit and return back to profile
     navigate('/profile')
   }
 
   return (
     <div role="contentinfo" className={styles.Edit__container}>
       <h2 className={styles.Edit__heading}>Edit Profile Information</h2>
-      <ProfileForm username={username} email={email} disable={false} />
+      <ProfileForm username={username} email={email} disable={false} ref={profileFormRef} />
       <Button
         text="Save"
         className={styles.Edit__button}
